23a6dd60729ebf12fc8196444742e9186b72139a
parMaster <tony.racomore@gmail.com>
2023-08-21 00:28:13 +0300

    integration example

---
+++ server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	tp := NewJwtProvider(5 * time.Minute)
+	service := NewAuthService(tp)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		if x := recover(); x != nil {
+			log.Printf("[WARN] run time panic:\n%v", x)
+			panic(x)
+		}
+
+		// catch signal and invoke graceful termination
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+		<-stop
+		log.Printf("[INFO] shutting down")
+		cancel()
+	}()
+
+	router := chi.NewRouter()
+	router.Mount("/auth", service.Handlers("/auth"))
+
+	httpServer := &http.Server{
+		Addr:              ":8000",
+		Handler:           router,
+		ReadHeaderTimeout: time.Second,
+		WriteTimeout:      30 * time.Second,
+		IdleTimeout:       time.Second,
+	}
+	log.Printf("[INFO] Listening: %s", "8000")
+
+	go func() {
+		<-ctx.Done()
+		if httpServer != nil {
+			if err := httpServer.Shutdown(ctx); err != nil {
+				log.Printf("[ERROR] failed to close http server, %v", err)
+			}
+		}
+	}()
+
+	httpServer.ListenAndServe()
+}
